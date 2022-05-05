dde1280aaf163e0ec976da735f0b58c4e17d728f
Damian <damian.mrozek@vp.pl>
2022-05-05 14:51:53 +0200

    anagrams 2nd solution

--- anagrams.py
+++ anagrams.py
@@ -1,22 +1,35 @@
 # 5.8.1.8 Anagrams
 
+# Rozwiązanie 1
+# tekst1 = str(input("Wprowadź pierwszy tekst: "))
+# tekst2 = str(input("Wprowadź drugi tekst: "))
+
+# tekst1 = tekst1.replace(' ', '').upper()
+# tekst2 = tekst2.replace(' ', '').upper()
+# lst = []
+
+# if tekst1 == '' or tekst2 == '':
+#     print('Nie anagramy')
+# else:
+#     for ch in tekst1:
+#         lst.append(ch)
+#     licznik = 0
+#     for ch in tekst2:
+#         if ch in lst:
+#             licznik += 1
+#     if licznik == len(lst):
+#         print('Anagramy')
+#     else:
+#         print('Nie anagramy')
+
+# Rozwiązanie 2
 tekst1 = str(input("Wprowadź pierwszy tekst: "))
 tekst2 = str(input("Wprowadź drugi tekst: "))
 
-tekst1 = tekst1.replace(' ', '').upper()
-tekst2 = tekst2.replace(' ', '').upper()
-lst = []
+lst1 = ''.join(sorted(list(tekst1.upper().replace(' ',''))))
+lst2 = ''.join(sorted(list(tekst2.upper().replace(' ',''))))
 
-if tekst1 == '' or tekst2 == '':
-    print('Nie anagramy')
+if len(lst1) > 0 and lst1 == lst2:
+    	print("Anagramy")
 else:
-    for ch in tekst1:
-        lst.append(ch)
-    licznik = 0
-    for ch in tekst2:
-        if ch in lst:
-            licznik += 1
-    if licznik == len(lst):
-        print('Anagramy')
-    else:
-        print('Nie anagramy')
+	print("Nie anagramy")	
