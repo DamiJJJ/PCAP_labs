1274faf94e9c29fb308227dbf5d4d0976fd23de0
Damian <damian.mrozek@vp.pl>
2022-05-19 00:34:23 +0200

    sorted by values and printed into file

--- char_frequency_histogram.py
+++ char_frequency_histogram.py
@@ -1,22 +1,38 @@
-# 6.9.1.15 Histogram częstotliwości znaków
+# 6.9.1.16 Histogram posortowanej częstotliwości znaków
 
 def histogram(file_name):
     
-    dict = {}
+    histogram = {}
     try:
         for line in open(file_name, 'r'):
             for char in line:
                 if char.isalpha():
                     char = char.lower()
-                    if char in dict:
-                        dict[char] += 1
+                    if char in histogram:
+                        histogram[char] += 1
                     else:
-                        dict[char] = 1
+                        histogram[char] = 1
     except IOError as error:
         print('Nie udało się otworzyć pliku!', error)
+        return
 
-    for letter, value in sorted(dict.items()):
-        print(letter, ' -> ', value)
+    # Sortowanie słownika po wartościach
+    list = sorted(histogram.items(), key=lambda x:x[1], reverse=True)
+    sortdict = dict(list)
+
+    try:
+        file = open(f"{file_name}.hist", 'w')
+        for letter, value in sortdict.items():
+            x = f"{letter} -> {value}"
+            print(x)
+            file.write(x)
+            file.write('\n')
+        file.close()
+        print('Zapisano do pliku!')
+    except Exception as error:
+        print('Nie udało się zapisać pliku!', error)
+        return
+
 
 def main():
 
